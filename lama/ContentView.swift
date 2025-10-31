ee187723d14cb6ffcb986fda96d3a5cc48fc2f96
Michal Jach <michaljach@gmail.com>
2025-10-31 23:11:15 +0100

    Initial Commit

---
+++ lama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lama
+//
+//  Created by Michal Jach on 31/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lama/lamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lamaApp.swift
+//  lama
+//
+//  Created by Michal Jach on 31/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct lamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
